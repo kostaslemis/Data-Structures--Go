63f349a862fdae47cd88e8e17c3995c556cebda7
Kostas Kordolaimis <kostasle@outlook.com>
2022-03-07 00:33:00 +0200

    Update linked list.go

--- linked list.go
+++ linked list.go
@@ -1,27 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type ListNode struct {
 	data int
 	next *ListNode
 }
 
-type linkedList struct {
-	head *ListNode
+type LinkedList struct {
+	head   *ListNode
+	tail   *ListNode
 	length int
 }
 
-func (list *linkedList) prepend(newNode *ListNode) {
+func (list *LinkedList) prepend(newNode *ListNode) {
 	second := list.head
 	list.head = newNode
 	list.head.next = second
+	if list.length == 0 {
+		list.tail = newNode
+	}
 	list.length++
 }
 
-func (list linkedList) printListData() {
+func (list *LinkedList) append(newNode *ListNode) {
+	list.tail.next = newNode
+	list.tail = newNode
+	list.length++
+}
+
+func (list *LinkedList) pop() (value int) {
+	if list.length == 0 {
+		return 0
+	} else if list.length == 1 {
+		value = list.head.data
+		list.head = nil
+		list.tail = nil
+	}
+
 	node := list.head
-	for  list.length !=0 {
+	for node.next != list.tail {
+		node = node.next
+	}
+
+	value = list.tail.data
+	list.tail = node
+	return
+}
+
+func (list LinkedList) printListData() {
+	node := list.head
+	for list.length != 0 {
 		fmt.Printf("%d ", node.data)
 		node = node.next
 		list.length--
@@ -29,7 +60,7 @@ func (list linkedList) printListData() {
 	fmt.Printf("\n")
 }
 
-func (list *linkedList) deleteValue(value int) {
+func (list *LinkedList) deleteValue(value int) {
 	if list.length == 0 {
 		return
 	}
@@ -49,11 +80,19 @@ func (list *linkedList) deleteValue(value int) {
 	}
 	node.next = node.next.next
 	list.length--
+	// Check tail
 }
 
+func (list *LinkedList) getLength() int {
+	return list.length
+}
+
+func (list *LinkedList) empty() bool {
+	return list.length == 0
+}
 
 func main() {
-	list := linkedList{}
+	list := LinkedList{}
 	node1 := &ListNode{data: 31}
 	node2 := &ListNode{data: 54}
 	node3 := &ListNode{data: 11}
@@ -64,4 +103,17 @@ func main() {
 
 	list.deleteValue(11)
 	list.printListData()
+
+	fmt.Println(list.getLength())
+	fmt.Println(list.empty())
+
+	node4 := &ListNode{data: 5}
+	list.append(node4)
+	list.printListData()
+	fmt.Println(list.getLength())
+
+	node5 := &ListNode{data: 100}
+	list.append(node5)
+	list.printListData()
+	fmt.Println(list.getLength())
 }
