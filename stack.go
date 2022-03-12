f2a3b28e9c78502cf9f9e3efca48a80f155f1c94
Kostas Kordolaimis <kostasle@outlook.com>
2022-03-12 18:09:48 +0200

    Stack Update

--- stack.go
+++ stack.go
@@ -2,24 +2,51 @@ package main
 
 import "fmt"
 
+// Item represents the data type of values stored in Stack
+type Item int
+
 // Stack represents a stack that holds a slice
 type Stack struct {
-	items []int
+	items []Item
 }
 
 // Push adds a value on top of the stack
-func (stack *Stack) Push(value int) {
+func (stack *Stack) Push(value Item) {
 	stack.items = append(stack.items, value)
 }
 
 //Pop removes a value from the top of the stack and returns the removed value
-func (stack *Stack) Pop() int {
-	l := len(stack.items)-1
+func (stack *Stack) Pop() Item {
+	l := len(stack.items) - 1
 	removedValue := stack.items[l]
 	stack.items = stack.items[:l]
 	return removedValue
 }
 
+func (stack *Stack) Peek() Item {
+	return stack.items[len(stack.items)-1]
+}
+
+func (stack *Stack) Contains(value Item) bool {
+	for i := range stack.items {
+		if value == stack.items[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func (stack *Stack) Size() int {
+	return len(stack.items)
+}
+
+func (stack *Stack) Clear() {
+	stack.items = nil
+}
+
+func (stack *Stack) Empty() bool {
+	return len(stack.items) == 0
+}
 
 func main() {
 	stack := Stack{}
@@ -29,7 +56,9 @@ func main() {
 	stack.Push(12)
 	stack.Push(13)
 	fmt.Println(stack)
+	fmt.Println(stack.Peek())
 
 	fmt.Println(stack.Pop())
 	fmt.Println(stack)
+	fmt.Println(stack.Peek())
 }
