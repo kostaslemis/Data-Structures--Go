6202e344dc083a032df04bdba50c29b5e082ea93
Kostas Kordolaimis <kostasle@outlook.com>
2022-03-12 17:48:47 +0200

    Queue Update

--- queue.go
+++ queue.go
@@ -2,23 +2,50 @@ package main
 
 import "fmt"
 
+// Item represents the data type of values stored in Queue
+type Item int
+
 // Queue represents a queue that holds a slice
 type Queue struct {
-	items []int
+	items []Item
 }
 
 // Enqueue adds a value at the end of the queue
-func (queue *Queue) Enqueue(value int) {
+func (queue *Queue) Enqueue(value Item) {
 	queue.items = append(queue.items, value)
 }
 
 // Dequeue removes a value from the end of the queue and returns the removed value
-func (queue *Queue) Dequeue() int {
+func (queue *Queue) Dequeue() Item {
 	removedValue := queue.items[0]
 	queue.items = queue.items[1:]
 	return removedValue
 }
 
+func (queue *Queue) Peek() Item {
+	return queue.items[0]
+}
+
+func (queue *Queue) Contains(value Item) bool {
+	for i := range queue.items {
+		if value == queue.items[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func (queue *Queue) Size() int {
+	return len(queue.items)
+}
+
+func (queue *Queue) Clear() {
+	queue.items = nil
+}
+
+func (queue *Queue) Empty() bool {
+	return len(queue.items) == 0
+}
 
 func main() {
 	queue := Queue{}
@@ -30,5 +57,8 @@ func main() {
 	fmt.Println(queue)
 
 	fmt.Println(queue.Dequeue())
+	fmt.Println(queue)
+
+	queue.Clear()
 	fmt.Println(queue)
 }
