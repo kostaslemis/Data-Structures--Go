9bb19e8983a0a7fc78d4095d0831a29ba15be348
Kostas Kordolaimis <kostasle@outlook.com>
2022-03-13 14:32:31 +0200

    Stack Update

--- stack.go
+++ stack.go
@@ -2,20 +2,19 @@ package main
 
 import "fmt"
 
-// Item represents the data type of values stored in Stack
-//type Item int
+// Item represents data type of values stored in Stack
+type Item int
 
-// Stack represents a stack that holds a slice
 type Stack struct {
 	items []Item
 }
 
-// Push adds a value on top of the Stack
+// Push adds value on top of Stack
 func (stack *Stack) Push(value Item) {
 	stack.items = append(stack.items, value)
 }
 
-// Pop removes a value from the top of the Stack and returns the removed value
+// Pop removes value from top of Stack and returns value
 func (stack *Stack) Pop() Item {
 	l := len(stack.items) - 1
 	removedValue := stack.items[l]
@@ -23,12 +22,12 @@ func (stack *Stack) Pop() Item {
 	return removedValue
 }
 
-// Peek returns the value from the top of the Stack
+// Peek returns value from top of Stack
 func (stack *Stack) Peek() Item {
 	return stack.items[len(stack.items)-1]
 }
 
-// Contains takes in a value and returns true if that value is stored in Stack
+// Contains takes in value and returns true if value is stored in Stack
 func (stack *Stack) Contains(value Item) bool {
 	for i := range stack.items {
 		if value == stack.items[i] {
@@ -38,7 +37,7 @@ func (stack *Stack) Contains(value Item) bool {
 	return false
 }
 
-// Size returns the size of the Stack
+// Size returns size of Stack
 func (stack *Stack) Size() int {
 	return len(stack.items)
 }
